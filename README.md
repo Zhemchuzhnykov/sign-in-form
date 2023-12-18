72fe324be9e9741b3b01d98afc4ccc302ce4b490
Yevhen <zhemchuzhnykov@ukr.net>
2023-12-18 19:41:52 +0200

    Applied Styled Component approach to styling components.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A standard log-in form with the fields "username" and "password". This project is dedicated to styling a React application: it does not represent any meaningful functinality development.
 
+## Important note
+
+This is a learning project, and it demonstrates different styling approaches within one project. The developer is fully aware that it is not a good practice to mix different styling approaches within one project but goes for it for learning and demo reasons.
+
 ## Concepts Learned During This Project
 
 - Styling a React app with vanilla CSS
@@ -11,3 +15,5 @@ A standard log-in form with the fields "username" and "password". This project i
 - Styling a React app with inline styles
 - Setting up styles dynamically with inline styles
 - CSS modules, scoping CSS rules
+- Styling with Styled Components package
+- Setting styles dynamically through component props with the Styled Components approach
--- src/components/AuthInput/AuthInputs.jsx
+++ src/components/AuthInput/AuthInputs.jsx
@@ -1,5 +1,24 @@
 import classes from "./AuthInputs.module.css";
 import { useState } from "react";
+import { styled } from "styled-components";
+
+// Examples of using styling with styled components
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -21,11 +40,23 @@ export default function AuthInputs() {
   const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
+  // Example of dynamic computing of classes
+  let emailInputClasses = classes["controls-input"];
+  let passwordInputClasses = classes["controls-input"];
+
+  if (emailNotValid) {
+    emailInputClasses += " " + classes["input-invalid"];
+  }
+
+  if (passwordNotValid) {
+    passwordInputClasses += " " + classes["input-invalid"];
+  }
+
   return (
-    <div className={classes.authInputs} id="auth-inputs">
-      <div className={classes.controls}>
+    <div className={classes["auth-inputs"]} id="auth-inputs">
+      <ControlContainer>
         <p>
-          <label className={emailNotValid ? classes.invalid : ""}>Email</label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <input
             type="email"
             // example of a dynamic inline styling
@@ -33,28 +64,26 @@ export default function AuthInputs() {
               backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
             }}
             // example of setting styles dynamically with a class
-            className={emailNotValid ? classes.invalid : undefined}
+            className={emailInputClasses}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label className={emailNotValid ? classes.invalid : ""}>
-            Password
-          </label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <input
             type="password"
             // example of a dynamic inline styling
             style={{
-              backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
+              backgroundColor: passwordNotValid ? "#fed2d2" : "#d1d5db",
             }}
             // example of setting styles dynamically with a class
-            className={passwordNotValid ? classes.invalid : undefined}
+            className={passwordInputClasses}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className={classes.actions}>
         <button type="button" className={classes.textButton}>
           Create a new account
